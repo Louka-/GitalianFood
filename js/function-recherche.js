87f084ab460f136cb315a987cd91c9a4fd24db2e
orianc <34635176+orianc@users.noreply.github.com>
2021-03-25 15:18:52 +0100

    Tentative de barre de recherche

---
+++ js/function-recherche.js
@@ -0,0 +1,10 @@
+
+console.log(`wigu + ${searchAsk}`)
+let searchAsk = document.getElementById("recherche").value;
+
+console.log(searchAsk);
+
+function readForm(params) {
+    console.log(searchAsk)
+    
+}
